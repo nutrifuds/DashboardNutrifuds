cd2126c5162faff6ccfaef763437dc80a94b2d48
Dwi Aji Cahyono <dwiaji724@gmail.com>
2023-06-15 21:01:13 +0700

    update typo

--- dashboardNutifuds - Copy/src/scripts/views/pages/resep.js
+++ dashboardNutifuds - Copy/src/scripts/views/pages/resep.js
@@ -4,7 +4,7 @@ import { createResepItemTemplate } from "../templates/template-creator";
 const resep = {
   async render() {
     return `
-      <h2 class="title">Cari Resep Sehat yang Anda, Disini</h2>
+      <h2 class="title">Cari Resep Sehat untuk Anda, Disini</h2>
 
       <div class="content-item"></div>
     `;
